454cb8f7fe4695a346956fe8a5489cea579c1eac
Ivan Rendon <ivansito87@gmail.com>
2019-09-12 11:03:14 -0400

    adds cmponent soingleton

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,21 +1,27 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
+import {CoursesService} from "./course/courses.service";
 
 
 @Component({
   selector: 'courses',
   template: `
       <h2>{{ "Title: " + getTitle()}}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+      <ul>
+          <li *ngFor="let course of courses">
+              {{ course }}
+          </li>
+      </ul>
   `
 })
 export class CoursesComponent {
-title = "First Component";
-courses = ["course1", "course2", "course3"];
-getTitle(){
-  return this.title;
-}
+  title = "First Component";
+  courses;
+
+  constructor(service: CoursesService){
+    this.courses = service.getCourses();
+  }
+
+  getTitle() {
+    return this.title;
+  }
 }
